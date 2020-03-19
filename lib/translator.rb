661153a5bda2c691df68d58bfaeca55a48a7dec7
Taevon Jordan <taevonjordan1@gmail.com>
2020-03-19 02:10:18 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,7 +12,12 @@ def load_library(file_path)
 end
 
 def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
-  # code goes here
+  library = load_library(file_path)
+  if library["get_emoticon"].include?(english_emoticon) 
+    library["get_emoticon"][english_emoticon]
+  else
+    "Sorry, that emoticon was not found"
+  end
 end
 
 def get_english_meaning(file_path = './lib/emoticons.yml', japanese_emoticon)
