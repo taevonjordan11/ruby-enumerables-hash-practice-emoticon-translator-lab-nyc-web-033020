dd7cca5ffd90f940d4ccc2df24ad286dee175164
Taevon Jordan <taevonjordan1@gmail.com>
2020-03-19 02:04:18 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,7 @@ def load_library(file_path)
   # code goes here
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
   # code goes here
 end
 
