8c5af3f09ecdb1015857362d11c8b8bde457546f
Taevon Jordan <taevonjordan1@gmail.com>
2020-03-19 02:04:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,6 +8,6 @@ def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
   # code goes here
 end
 
-def get_english_meaning
+def get_english_meaning(file_path = './lib/emoticons.yml', japanese_emoticon)
   
 end
